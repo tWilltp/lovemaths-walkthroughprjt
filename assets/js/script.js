bf9564bc15741ad156b96b09fbc2bc6c10f0e92d
tWilltp <williamjenkinsfeb@gmail.com>
2022-09-20 16:17:08 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', function()) {
          }
         })
     }
+
+    runGame('addition')
+
 }
 
 /**
@@ -19,11 +22,18 @@ document.addEventListener('DOMContentLoaded', function()) {
  * and after the user's answer has been processed
  */
 
-function runGame () {
+function runGame ( gameType) {
 
     // creates 2 random numbers between 1 and 25
     let num1 = Math.floor(math.random() * 25) + 1;
     let num2 = Math.floor(math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.Aborting!`;
+    }
 }
 
 function checkAnswer () {
@@ -42,7 +52,11 @@ function incrementWrongAnswer () {
 
 }
 
-function displayAdditionQuestion () {
+function displayAdditionQuestion (operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
