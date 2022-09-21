eed959b3affdd166905da68ca629511755649e6a
tWilltp <williamjenkinsfeb@gmail.com>
2022-09-21 09:37:59 +0000

    add increment score and increment wrong answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer () {
 
     if (isCorrect) {
         alert("hey you got it right!");
+        incrementScore();
     } else {
         alert(`you answered ${userAnswer}. the correct answer was ${calculatedAnswer}[0]`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -72,12 +74,22 @@ function calculateCorrectAnswer () {
     }
 }
 
-function incrementScore () {
+/**
+ * gets the current score from the DOM and increments it by 1
+ */
 
+function incrementScore () {
+     let oldScore = parseInt(document.getElementById('score').innerText);
+     document.getElementById('score').innerText = ++oldScore;
 }
 
-function incrementWrongAnswer () {
+/**
+ * gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 
+function incrementWrongAnswer () {
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion (operand1, operand2) {
